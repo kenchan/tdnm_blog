0ac68860a5c2d11f9806065a180acf8efb8ecb4f
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2015-09-15 23:57:55 +0900

    rails g monban:scaffold and remove user resource controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = authenticate_session(session_params)
+
+    if sign_in(user)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
+  resource :session, only: [:new, :create, :destroy]
   resources :articles, only: %w(index show)
   get '/atom.xml', controller: 'articles', action: 'index', format: 'atom'
   get '/:year/:month/:day/:title', to: 'articles#show', year: /\d{4}/, month: /\d{2}/, day: /\d{2}/
