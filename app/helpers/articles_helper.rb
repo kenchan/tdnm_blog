3e094d8ef086239cbe722bea5cd6aec25c043121
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2016-01-12 23:29:42 +0900

    Refactor article_url method

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,16 +1,17 @@
 module ArticlesHelper
-  def article_url(article)
+  def article_url(article, opts = {})
     d = article.published_on
 
     url_for(
-      controller: 'articles',
-      action: 'show',
-      year: d.year,
-      month: '%02d' % d.month,
-      day: '%02d' % d.day,
-      title: article.url_title,
-      format: 'html',
-      only_path: false
+      {
+        controller: 'articles',
+        action: 'show',
+        year: d.year,
+        month: '%02d' % d.month,
+        day: '%02d' % d.day,
+        title: article.url_title,
+        format: 'html'
+      }.merge(opts)
     )
   end
 end
