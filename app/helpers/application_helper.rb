7fe6de150170e70dc08669866e53d240730a3b05
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2016-04-27 23:59:02 +0900

    truncate aritlce body fot meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,24 @@ module ApplicationHelper
     @_weblog ||= Weblog.first
   end
 
+  def meta_description
+    @_meta_description ||= if @article
+      truncate(
+        strip_tags(::GitHub::Markup::Markdown.new.render(@article.body)),
+        length: MetaTags.config.description_limit
+      )
+    else
+      nil
+    end
+  end
+
   def default_meta_tags
     {
       site: weblog.title,
       charset: 'utf-8',
       title: @article.try(:title),
-      reverse: true
+      reverse: true,
+      description: meta_description
     }
   end
 end
