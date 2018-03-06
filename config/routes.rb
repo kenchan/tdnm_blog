7230733282d77fe0b04915dc5822f23e12138ed4
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-03-06 20:24:12 +0900

    remove monban and SessionController

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
-  resource :session, only: [:new, :create, :destroy]
   resources :articles, only: %w(index show)
   get '/atom.xml', controller: 'articles', action: 'index', format: 'atom'
   get '/:year/:month/:day/:title', to: 'articles#show', year: /\d{4}/, month: /\d{2}/, day: /\d{2}/
