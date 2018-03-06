cdf18b9dc1bacedb91c8c4f085412bd3f11ee1e3
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-03-06 22:12:01 +0900

    http basic auth

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,10 +6,11 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
-
-    def authenticate_admin
-      # TODO Add authentication logic here.
+    if Rails.application.secrets.admin_username
+      http_basic_authenticate_with(
+        name: Rails.application.secrets.admin_username,
+        password: Rails.application.secrets.admin_password
+      )
     end
 
     # Override this value to specify the number of elements to display at a time
