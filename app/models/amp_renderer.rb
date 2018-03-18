f962625f5c8955a442390b031e8c72be8b29fe32
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-03-19 00:46:48 +0900

    render amp-twitter

--- app/models/amp_renderer.rb
+++ app/models/amp_renderer.rb
@@ -1,8 +1,11 @@
 class AmpRenderer < CommonMarker::HtmlRenderer
   def html(node)
     block do
-      if node.string_content =~ %r[https://www.instagram.com/p/([a-zA-Z0-9]+)/]
+      case node.string_content
+      when %r[https://www.instagram.com/p/([a-zA-Z0-9]+)/]
         out(%(<amp-instagram data-shortcode="#{$1}" data-captioned width="400" height="400" layout="responsive"></amp-instagram>))
+      when %r[https://twitter.com/kenchan/status/([0-9]+)]
+        out(%(<amp-twitter width="375" height="472" layout="responsive" data-tweetid="#{$1}"></amp-twitter>))
       else
         super(node)
       end
