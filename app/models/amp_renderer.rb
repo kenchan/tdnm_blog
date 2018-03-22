4f9d1fb8583be6d1a89f9534d313865018309860
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-03-23 00:16:26 +0900

    fixed size amazon link

--- app/models/amp_renderer.rb
+++ app/models/amp_renderer.rb
@@ -6,8 +6,8 @@ class AmpRenderer < CommonMarker::HtmlRenderer
         out(%(<amp-instagram data-shortcode="#{$1}" data-captioned width="400" height="400" layout="responsive"></amp-instagram>))
       when %r[https://twitter.com/kenchan/status/([0-9]+)]
         out(%(<amp-twitter width="375" height="472" layout="responsive" data-tweetid="#{$1}"></amp-twitter>))
-      when %r{<iframe.+width:([0-9]+).+height:([0-9]+).+src=["'](//rcm-fe.amazon-adsystem.com[^"']+)["']}
-        out(%(<amp-iframe width="#{$1}" height="#{$2}" title="amazon" layout="fixed" sandbox="allow-scripts allow-same-origin allow-popups" src="https:#{$3}">Amazon Associate link</amp-iframe>))
+      when %r{<iframe.+(//rcm-fe.amazon-adsystem.com[^"']+)+}
+        out(%(<amp-iframe width="120" height="240" title="amazon" layout="fixed" sandbox="allow-scripts allow-same-origin allow-popups" src="https:#{$1}">Amazon Associate link</amp-iframe>))
       when %r{<iframe[^>]+https://www.youtube.com/embed/([a-zA-Z0-9]+)}
         out(%(<amp-youtube data-videoid="#{$1}" width="480" height="270" layout="responsive"></amp-youtube>))
       else
