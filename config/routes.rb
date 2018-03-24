d316886fe24fab71860f6058581a2f595ce21ede
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-03-24 23:52:51 +0900

    rediret old url to new one

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   resources :articles, only: %w(index show)
   get '/atom.xml', controller: 'articles', action: 'index', format: 'atom'
   get '/:year/:month/:day/:title', to: 'articles#show', year: /\d{4}/, month: /\d{2}/, day: /\d{2}/
-  get '/blog/:year/:month/:day/:title', to: 'articles#show', year: /\d{4}/, month: /\d{2}/, day: /\d{2}/
+  get '/blog/:year/:month/:day/:title', to: redirect('/%{year}/%{month}/%{day}/%{title}')
 end
