e5a17ac9decf6a0c45e4873fb6a85452b2497516
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-04-01 23:21:10 +0900

    add to sitemap.xml only published articles

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,7 @@
 SitemapGenerator::Sitemap.default_host = ENV['DEFAULT_HOST'] || "http://www.example.com"
 
 SitemapGenerator::Sitemap.create do
-  Article.find_each do |a|
+  Article.published.find_each do |a|
     d = a.published_on
     add(
       url_for(
