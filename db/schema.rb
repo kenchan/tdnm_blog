2f73f69ecc70708eca68a00dff08e872f920dfa6
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-04-17 00:10:54 +0900

    Add column eye-catching image url to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416143233) do
+ActiveRecord::Schema.define(version: 20180416150121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180416143233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "draft", default: false, null: false
+    t.string "eye_catching_image_url", default: "", null: false
   end
 
   create_table "weblogs", force: :cascade do |t|
