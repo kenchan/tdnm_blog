da6abd405bbb446cd7ec43653f98a15f74342f7e
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-02-02 23:54:01 +0900

    Return 404 if articles not found

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,6 @@ class ArticlesController < ApplicationController
     year, month, day = params.values_at(:year, :month, :day).map(&:to_i)
     slug = params[:slug]
 
-    @article = Article.published.where(published_on: Date.new(year, month, day), slug: slug).first
+    @article = Article.published.where(published_on: Date.new(year, month, day), slug: slug).first!
   end
 end
