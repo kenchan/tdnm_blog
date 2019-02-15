619cc19ec16bc4319135dbc7f8b1420ead73c5ed
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-02-15 21:51:30 +0900

    Article attributes are not null

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -1,8 +1,8 @@
 module Types
   class ArticleType < Types::BaseObject
-    field :title, String, null: true
-    field :body, String, null: true
-    field :slug, String, null: true
-    field :published_on, Types::DateType, null: true
+    field :title, String, null: false
+    field :body, String, null: false
+    field :slug, String, null: false
+    field :published_on, Types::DateType, null: false
   end
 end
