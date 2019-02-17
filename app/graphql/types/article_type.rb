fb75edee3e3556bab635b971553bf64354d06ba4
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-02-17 16:47:01 +0900

    Query articles by ID

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -1,8 +1,9 @@
 module Types
   class ArticleType < Types::BaseObject
+    field :id, ID, null: false
     field :title, String, null: false
     field :body, String, null: false
     field :slug, String, null: false
-    field :published_on, Types::DateType, null: false
+    field :published_on, String, null: false
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :article, Types::ArticleType, null: true do
+      description 'Find a article by ID'
+      argument :id, ID, required: true
+    end
+
+    def article(id:)
+      Article.find(id)
     end
   end
 end
