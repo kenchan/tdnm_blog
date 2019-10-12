1f21e3a63d168db62d85763ba583e31abcb8ebdb
kenchan <kenichi.taka@gmail.com>
2019-10-12 23:01:09 +0900

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
-  resources :articles, only: %w(index show)
   get '/atom.xml', controller: 'articles', action: 'index', format: 'atom'
   get '/:year/:month/:day/:slug', to: 'articles#show', year: /\d{4}/, month: /\d{2}/, day: /\d{2}/
   get '/:year/:month/:day', to: 'articles#list', year: /\d{4}/, month: /\d{2}/, day: /\d{2}/
