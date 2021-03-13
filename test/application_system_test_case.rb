41028f68d5981907b5b42da79b11e3d38f649320
Kenichi Takahashi <kenichi.taka@gmail.com>
2021-03-13 18:48:13 +0900

    run system:test on GitHub Actions

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium_remote_firefox
+  driven_by Capybara.default_driver
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,9 +12,13 @@ Capybara.register_driver :selenium_remote_firefox do |app|
   Capybara::Selenium::Driver.new(
     app,
     browser: :remote,
-    url: "http://#{ENV['SELENIUM_HOST']}:4444/wd/hub",
+    url: "http://#{ENV['SELENIUM_HOST'] || 'localhost' }:4444/wd/hub",
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox
   )
 end
 
-Capybara.server_host = ENV['SELENIUM_APP_HOST']
+Capybara.default_driver = ENV['CI'] ? :selenium_headless : :selenium_remote_firefox
+
+if ENV['SELENIUM_APP_HOST']
+  Capybara.server_host = ENV['SELENIUM_APP_HOST']
+end
