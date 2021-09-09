b269ecd48cbb0f16fb2895e53563d68dee657411
Kenichi Takahashi <kenichi.taka@gmail.com>
2021-09-09 22:47:39 +0900

    oops

--- db/migrate/20210908153117_add_unique_index_to_article_title.rb
+++ db/migrate/20210908153117_add_unique_index_to_article_title.rb
@@ -4,4 +4,3 @@ class AddUniqueIndexToArticleTitle < ActiveRecord::Migration[6.1]
   end
 end
 
-Article.pluck(:title).tally.select {|t, i| i >= 2}.map(&:first).each {|t| Article.where(title: t).each {|a| a.update(title: "#{a.published_on}: #{a.title}") } }
