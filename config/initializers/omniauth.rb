aa6e03cc7ab8da137854662f286e8c8e9479f55e
Kenichi Takahashi <kenichi.taka@gmail.com>
2022-05-10 09:20:38 +0900

    Use SIWE except Rails.env.test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,18 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  issuer = 'https://oidc.login.xyz/'
-  client_options = {
-    scheme: 'https',
-    host: 'oidc.login.xyz',
-    port: 443,
-    authorization_endpoint: '/authorize',
-    token_endpoint: '/token',
-    userinfo_endpoint: '/userinfo',
-    jwks_uri: '/jwk',
-    identifier: Rails.application.credentials[Rails.env.to_sym][:siwe_client_id],
-    secret: Rails.application.credentials[Rails.env.to_sym][:siwe_client_secret]
-  }
+  unless Rails.env.test?
+    issuer = 'https://oidc.login.xyz/'
+    client_options = {
+      scheme: 'https',
+      host: 'oidc.login.xyz',
+      port: 443,
+      authorization_endpoint: '/authorize',
+      token_endpoint: '/token',
+      userinfo_endpoint: '/userinfo',
+      jwks_uri: '/jwk',
+      identifier: Rails.application.credentials[Rails.env.to_sym][:siwe_client_id],
+      secret: Rails.application.credentials[Rails.env.to_sym][:siwe_client_secret]
+    }
 
-  provider :siwe, issuer: issuer, client_options: client_options
+    provider :siwe, issuer: issuer, client_options: client_options
+  end
 end
