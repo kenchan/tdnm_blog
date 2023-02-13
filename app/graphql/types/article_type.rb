4ab8f827328c3b987ab410cf03fa479a79e95d12
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-02-14 01:08:30 +0900

    Add datetime fields to graphql api

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -4,6 +4,8 @@ module Types
     field :title, String, null: false
     field :body, String, null: false
     field :slug, String, null: false
-    field :published_on, String, null: false
+    field :published_on, GraphQL::Types::ISO8601DateTime, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
